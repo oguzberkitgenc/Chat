47b5d5aa5ea2377379c93c63422c60f4816d8456
Oğuz Berkit Genç <75220168+golgeroot@users.noreply.github.com>
2023-06-20 12:46:37 +0300

    ..

--- Chat/Home.cs
+++ Chat/Home.cs
@@ -20,21 +20,15 @@ namespace Chat
         }
         Connection bgl = new Connection();
         SqlCommand cmd;
-        DataTable dt, yeniTablo;
         SqlDataReader dr;
-        DataRow drs, yeniSatir;
         public string userName = "";
         int kontrol = 0;
         int degismismi = 0;
         int karakter = 0;
         string kelimeBol = "";
-        bool ColumnWidth = false;
 
         private void Home_Load(object sender, EventArgs e)
         {
-            dt = new DataTable();
-            dt.Columns.Add(new DataColumn("Kullanici", typeof(string)));
-            dt.Columns.Add(new DataColumn("Mesaj", typeof(string)));
             timer1.Start();
             SqlConnection con2 = new SqlConnection(bgl.Adres);
             con2.Open();
@@ -46,113 +40,38 @@ namespace Chat
                 break;
             }
             con2.Close();
-            //con2.Open();
-            //cmd = new SqlCommand("SELECT [Kullanici],[Mesaj] FROM MESSAGE", con2);
-            //dr = cmd.ExecuteReader();
-            //drs = null;
-            //while (dr.Read())
-            //{
-            //    if (userName != dr[0].ToString())
-            //    {
-            //        drs = dt.NewRow();
-            //        drs["Kullanici"] = dr[0].ToString();
-            //        drs["Mesaj"] = dr[1].ToString();
-            //        dt.Rows.Add(drs);
-            //    }
-            //    else
-            //    {
-            //        drs = dt.NewRow();
-            //        drs["Kullanici"] = dr[1].ToString();
-            //        drs["Mesaj"] = dr[0].ToString();
-            //        dt.Rows.Add(drs);
-            //    }
-            //}
-            //gridControl1.DataSource = dt;
+
             SiraliListele();
-
-            //con2.Close();
-
             gridView1.MoveLast();
-            //gridView1.BestFitColumns();
+
             kontrol = degismismi;
             gridView1.OptionsView.ShowHorizontalLines = DevExpress.Utils.DefaultBoolean.False;
             gridView1.OptionsView.ShowVerticalLines = DevExpress.Utils.DefaultBoolean.False;
-
         }
         void SiraliListele()
         {
-
             SqlConnection con = new SqlConnection(bgl.Adres);
             con.Open();
-
             // Değişiklik kontrolü için mevcut değeri al
             cmd = new SqlCommand("SELECT [S] FROM SAY", con);
             int degismismi = Convert.ToInt32(cmd.ExecuteScalar());
-
             con.Close();
 
             if (kontrol != degismismi)
             {
                 con.Open();
-
                 // MESSAGE tablosundan verileri al
                 cmd = new SqlCommand("SELECT [Kullanici],[Mesaj] FROM MESSAGE", con);
                 dr = cmd.ExecuteReader();
-
                 DataTable dt = new DataTable();
                 dt.Load(dr);
+                gridControl1.DataSource= dt;
                 con.Close();
-
-                DataTable yeniTablo = new DataTable();
-                yeniTablo.Columns.Add("Kullanici", typeof(string));
-                yeniTablo.Columns.Add("Mesaj", typeof(string));
-
-                foreach (DataRow row in dt.Rows)
-                {
-                    string kullanici = row["Kullanici"].ToString();
-                    string mesaj = row["Mesaj"].ToString();
-                    //if (userName == kullanici)
-                    //{
-                    yeniTablo.Rows.Add(mesaj, kullanici);
-                    //    ColumnWidth=true;
-                    //}
-                    //else
-                    //{
-                    //    yeniTablo.Rows.Add(kullanici,mesaj);
-                    //}
-                }
-                gridControl1.DataSource = yeniTablo;
                 kontrol = degismismi;
                 gridView1.Columns[1].Width = 40;
                 gridView1.MoveLast();
             }
         }
-        //void EskiListele()
-        //{
-        //    SqlConnection con2 = new SqlConnection(bgl.Adres);
-        //    con2.Open();
-        //    cmd = new SqlCommand("SELECT [S] FROM SAY", con2);
-        //    dr = cmd.ExecuteReader();
-        //    while (dr.Read())
-        //    {
-        //        degismismi = Convert.ToInt16(dr[0]);
-        //    }
-        //    con2.Close();
-        //    if (kontrol != degismismi)
-        //    {
-        //        con2.Open();
-        //        dt = new DataTable();
-        //        cmd = new SqlCommand();
-        //        cmd.CommandText = "SELECT [ID],[Kullanici],[Mesaj] FROM MESSAGE";
-        //        cmd.Connection = con2;
-        //        da = new SqlDataAdapter(cmd);
-        //        da.Fill(dt);
-        //        gridControl1.DataSource = dt;
-        //        con2.Close();
-        //        kontrol = degismismi;
-        //        gridView1.MoveLast();
-        //    }
-        //}
         private void BGonder_Click(object sender, EventArgs e)
         {
             try
@@ -166,7 +85,6 @@ namespace Chat
                     int lentgh = Math.Min(80, karakter - startIndex);
                     kelimeBol = richTextBox1.Text.Substring(startIndex, lentgh);
                     con.Open();
-                    dt = new DataTable();
                     cmd = new SqlCommand();
                     cmd.CommandText = "INSERT INTO MESSAGE ([Kullanici],[Mesaj]) values (@p1,@p2)";
                     cmd.Connection = con;
@@ -186,7 +104,6 @@ namespace Chat
             }
 
         }
-
         private void gridView1_RowCellStyle(object sender, RowCellStyleEventArgs e)
         {
             GridView view = sender as GridView;
@@ -205,10 +122,10 @@ namespace Chat
             //    e.Appearance.ForeColor = Color.Black;
             //}
         }
-
         private void simpleButton2_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Henüz aktif değil");
+            Setting s = new Setting();
+            s.UyariGonder("test");
         }
 
         private void simpleButton3_Click(object sender, EventArgs e)
@@ -216,25 +133,21 @@ namespace Chat
             MessageBox.Show("Henüz aktif değil");
 
         }
-
         private void simpleButton4_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Henüz aktif değil");
 
         }
-
         private void simpleButton6_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Henüz aktif değil");
 
         }
-
         private void timer1_Tick_1(object sender, EventArgs e)
         {
             SiraliListele();
 
         }
-
         private void BCikis_Click(object sender, EventArgs e)
         {
             Application.Exit();
---
+++ Chat/Setting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat
+{
+    public class Setting
+    {
+
+        public void UyariGonder(string Msg)
+        {
+            System.Windows.Forms.MessageBox.Show(Msg.ToString());
+        }
+    }
+}
